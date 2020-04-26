f1f34f78cf4d02e1671fc0689387e3dfd373e609
Nalepsky <nalepka.marek@student.pk.edu.pl>
2020-04-26 19:46:30 +0200

    #18 add modal with recipe details

--- src/components/RecipesSearchGeneral/RecipeComponent.vue
+++ src/components/RecipesSearchGeneral/RecipeComponent.vue
@@ -1,47 +1,52 @@
 <template>
   <div class="RecipeComponent">
-    <b-modal ref="recipe-modal" size="lg" centered  hide-footer :title= recipeTitle>
-      <div v-if="this.steps" class="d-block text-left" >
-        <ul id="example-1" style="list-style-type:none">
-          <li v-for="el in this.steps" :key="el"><b>{{el.number}}</b>: {{ el.step }}</li>
-        </ul>
+    <b-modal ref="recipe-modal" size="lg" centered hide-footer :title="recipeTitle">  
+      <div>
+        <RecipeDetailComponent  
+        v-bind:id=this.id 
+        v-bind:units=this.units 
+        v-bind:steps=this.steps 
+        v-bind:ingredients=this.ingredients 
+        v-bind:image=this.image
+        />
+        <b-button variant="outline-info" block @click="changeUnits">{{changeUnitButtonName}}</b-button>
+        <b-button variant="outline-danger" block @click="hideModal">Close</b-button>
       </div>
-      <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Close</b-button>
     </b-modal>
-    <b-card
-      v-bind:title="title"
-      v-bind:img-src="imageUrl"
-      img-alt="Image"
-      img-top
-      class="mb-1 mr-1"
-      style="min-width: 15rem; max-width: 20rem; height: 37rem"
-      img-height="300rem"
-    >
+    <b-card v-bind:title="title"
+            v-bind:img-src="imageUrl"
+            img-alt="Image"
+            img-top
+            class="mb-1 mr-1"
+            style="min-width: 15rem; max-width: 20rem; height: 37rem" img-height="300rem">
       <!--      <b-img :src="imageUrl" center fluid rounded="10"/>-->
       <b-card-text>
         <p class="information">Ready in minutes: {{readyInMinutes}}</p>
         <p class="information">Servings: {{servings}}</p>
         <p class="information">Likes: {{likes}} <button v-on:click="setLikes(id, likes+1)">Add Like</button></p>
       </b-card-text>
-      <b-button
-        href="#"
-        variant="primary"
-        v-on:click="showRecipe"
-        style="color: aliceblue"
-      >Go to recipe</b-button>
+      <b-button href="#" variant="primary" style="color: aliceblue" v-on:click="showRecipe">Go to recipe</b-button>
     </b-card>
   </div>
 </template>
 
 <script>
-  import {baseRecipiesApiPhotosUrl} from '../../shared/constants';
+  import {baseRecipiesApiPhotosUrl, getWithEndpoint, baseRecipiesApiUrl} from '../../shared/constants';
   import {getLikes, setLikes} from '../../shared/DBHandling';
+  import RecipeDetailComponent from "./RecipeDetailComponent.vue";
+  import axios from "axios";
 
   export default {
+    components: {RecipeDetailComponent,},
     data() {
       return {
         results: [],
-        likes: 0
+        steps: [],
+        ingredients: [],
+        likes: 0,
+        recipeTitle: "",
+        units: "metric",
+        changeUnitButtonName: "change units to us"
       }
     },
     props: {
@@ -61,17 +66,7 @@
       getLikes(this.id)
       .then(likes => this.likes = likes);
     },
-    showRecipe: function() {
-      axios
-        .get(
-          getWithEndpoint(
-            new URL(`${baseRecipiesApiUrl}/${this.id}/analyzedInstructions`)
-          )
-        )
-        .then(response => (this.steps = response.data[0].steps));
-
-      this.recipeTitle = this.title;
-      this.$refs["recipe-modal"].show();
+    created() {
     },
     methods: {
       print: function () {
@@ -80,8 +75,39 @@
       setLikes: function(id, likes){
         setLikes(id, likes)
         .then(likes => this.likes = likes);
+      },
+      showRecipe: function() {   
+        axios
+          .get(
+            getWithEndpoint(
+              new URL(`${baseRecipiesApiUrl}/${this.id}/analyzedInstructions`)
+            )
+          )
+          .then(response => (this.steps = response.data[0].steps));
+
+        axios
+          .get(
+            getWithEndpoint(
+              new URL(`${baseRecipiesApiUrl}/${this.id}/ingredientWidget.json`)
+            )
+          )
+          .then(response => (this.ingredients = response.data.ingredients));
+
+        this.recipeTitle = this.title;
+        this.$refs["recipe-modal"].show();
+      },
+      hideModal() {
+      this.$refs["recipe-modal"].hide();
+      },
+      changeUnits() {
+        if (this.units === "metric") {
+          this.units = "us";
+          this.changeUnitButtonName = "change units to metric";
+        } else {
+          this.units = "metric";
+          this.changeUnitButtonName = "change units to us";
+        }
       }
     }
   }
-};
 </script>
